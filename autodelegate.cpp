2748d8ad8e7cf180fa30b468518fd346667ae7aa
DaGardner <dagardner@protonmail.com>
2022-07-14 10:04:50 +0200

    chore: format with clang-format

--- autodelegate.cpp
+++ autodelegate.cpp
@@ -1,88 +1,69 @@
-#include <eosio/eosio.hpp>
 #include <eosio/asset.hpp>
+#include <eosio/eosio.hpp>
 
-CONTRACT autodelegate : public eosio::contract
-{
-				public:
-								using eosio::contract::contract;
+CONTRACT autodelegate : public eosio::contract {
+public:
+  using eosio::contract::contract;
 
-								[[eosio::on_notify("eosio.token::transfer")]] void receive_token_transfer(
-																eosio::name & from,
-																eosio::name & to,
-																eosio::asset & quantity,
-																std::string & memo)
-								{
-												if (from == get_self() || to != get_self())
-												{
-																return;
-												}
+  [[eosio::on_notify("eosio.token::transfer")]] void receive_token_transfer(
+      eosio::name & from, eosio::name & to, eosio::asset & quantity,
+      std::string & memo) {
+    if (from == get_self() || to != get_self()) {
+      return;
+    }
 
+    if (memo.rfind("stake:", 0) != 0) {
+      return;
+    }
 
-												if (memo.rfind("stake:", 0) != 0)
-												{
-																return;
-												}
+    auto adu = autodelusers(get_self(), get_self().value);
+    adu.require_find(from.value, "User is not on the allow list.");
 
-												auto adu = autodelusers(get_self(), get_self().value);
-												adu.require_find(from.value, "User is not on the allow list.");
+    eosio::check(get_first_receiver() == eosio::name("eosio.token"),
+                 "Invalid token contract");
 
-												eosio::check(get_first_receiver() == eosio::name("eosio.token"), "Invalid token contract");
+    eosio::name target = eosio::name(memo.erase(0, 6));
 
+    eosio::asset net_bw;
+    net_bw.amount = 0;
+    net_bw.symbol = quantity.symbol;
 
-												eosio::name target = eosio::name(memo.erase(0, 6));
+    eosio::asset cpu_bw;
+    cpu_bw.amount = quantity.amount;
+    cpu_bw.symbol = quantity.symbol;
 
-												eosio::asset net_bw;
-												net_bw.amount = 0;
-												net_bw.symbol = quantity.symbol;
+    eosio::action(eosio::permission_level{get_self(), eosio::name("del")},
+                  eosio::name("eosio"), eosio::name("delegatebw"),
+                  std::make_tuple(get_self(), target, net_bw, cpu_bw, false))
+        .send();
+  }
 
-												eosio::asset cpu_bw;
-												cpu_bw.amount = quantity.amount;
-												cpu_bw.symbol = quantity.symbol;
+  ACTION adduser(eosio::name user) {
+    require_auth(get_self());
+    auto adu = autodelusers(get_self(), get_self().value);
+    auto it = adu.find(user.value);
 
-												eosio::action(
-																				eosio::permission_level{get_self(), eosio::name("del")},
-																				eosio::name("eosio"),
-																				eosio::name("delegatebw"),
-																				std::make_tuple(
-																								get_self(),
-																								target,
-																								net_bw,
-																								cpu_bw,
-																								false)
-																		 ).send();
-								}
+    if (it != adu.end()) {
+      return;
+    }
+    adu.emplace(get_self(), [&](auto &row) { row.user = user; });
+  }
 
-								ACTION adduser(eosio::name user)
-								{
-												require_auth(get_self());
-												auto adu = autodelusers(get_self(), get_self().value);
-												auto it = adu.find(user.value);
+  ACTION rmuser(eosio::name user) {
+    require_auth(get_self());
 
-												if (it != adu.end())
-												{
-																return;
-												}
-												adu.emplace(get_self(), [&](auto &row)
-																				{
-																				row.user = user;
-																				});
+    auto adu = autodelusers(get_self(), get_self().value);
+    auto e = adu.require_find(user.value, "User is not on the allow list.");
 
-								}
+    adu.erase(e);
+  }
 
-								ACTION rmuser(eosio::name user)
-								{
-												require_auth(get_self());
+private:
+  struct [[eosio::table("autodelusers")]] autodeluser {
+    eosio::name user;
 
-												auto adu = autodelusers(get_self(), get_self().value);
-												auto e = adu.require_find(user.value, "User is not on the allow list.");
-
-												adu.erase(e);
-								}
-				private:
-								struct [[eosio::table("autodelusers")]] autodeluser {
-												eosio::name user;
-
-												auto primary_key() const { return user.value; };
-								};
-								typedef eosio::multi_index<eosio::name("autodelusers"), autodeluser> autodelusers;
+    auto primary_key() const { return user.value; };
+  };
+  typedef eosio::multi_index<eosio::name("autodelusers"), autodeluser>
+      autodelusers;
 };
